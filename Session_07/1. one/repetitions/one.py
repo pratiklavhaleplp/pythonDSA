09a4119f78e70d3acbc278b13cc696b78d3427f6
praitklavhale <102862714+pratiklavhaleplp@users.noreply.github.com>
2025-01-28 23:43:47 +0530

    3rd commit

---
+++ Session_07/1. one/repetitions/one.py
@@ -0,0 +1,137 @@
+# one
+print('start')
+N = 5
+i = 0
+
+while i < N:
+  print("Outer loop start with i : ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i}, j: {j}')
+    j = j + 1
+  i = i + 1
+print("end")
+
+
+# two
+print("start")
+N = 5
+i = 0
+
+while i < 5:
+  print("Outer loop starts with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i} j: {j}')
+    j = j + 1
+  i = i + 1
+print("end")
+
+
+# three
+print('start')
+N = 5
+i = 0
+
+while i < 5: 
+  print("Outer loop start with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i}, j: {j}')
+    j = j + 1
+  i = i + 1
+print("end")
+
+# four
+print("start")
+N = 5
+i = 0
+while i < N:
+  print("Outher loop start with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i}, j: {j}')
+    j = j + 1
+  i = i + 1
+print("end")
+
+#five
+print("start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop starts with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i}, j: {j}')
+    j = j + 1
+  i = i+1
+print("End")
+
+#six
+print("Start")
+N = 5
+i = 0
+while i< N:
+  print("Outer loop starts with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i}, j: {j}')
+    j = j +1 
+  i = i + 1
+print("End")
+
+#seven
+print("start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop starts with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i}, j: {j}')
+    j = j+1
+  i = i + 1
+print("End")
+
+# eight
+print("Start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop start with i : ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i} j: {j}')
+    j = j + 1
+  i = i + 1
+print("End")
+
+
+# nine
+print("Start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop start with  i : ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i}, j: {j}')
+    j = j + 1
+  i = i + 1
+print("End")
+
+
+
+# ten 
+print("Start")
+N = 5
+i = 0
+while i < N:
+  print("outer loop start with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i}, j:{j}')
+    j = j + 1
+  i = i + 1
+print("End")
