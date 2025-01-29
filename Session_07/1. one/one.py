d034fff0e27ef6b0ae25e584e4b10d2e7312a669
praitklavhale <102862714+pratiklavhaleplp@users.noreply.github.com>
2025-01-29 06:56:35 +0530

    new change

--- Session_07/1. one/one.py
+++ Session_07/1. one/one.py
@@ -1,12 +1,11 @@
-print('start')
+print("Start")
 N = 5
 i = 0
-
 while i < N:
-  print("Outer loop start with i : ", i)
+  print("Outer loop variable start with i: ", i)
   j = 0
   while j < N:
-    print(f'i: {i}, j: {j}')
+    print(f'i: {i} j: {j}')
     j = j + 1
   i = i + 1
-print("end")
+print("End")
--- Session_07/1. one/repetitions/one.py
+++ Session_07/1. one/repetitions/one.py
@@ -135,3 +135,142 @@ while i < N:
     j = j + 1
   i = i + 1
 print("End")
+
+
+
+# 11
+print("start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop starts with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i}, j: {j}')
+    j = j + 1
+  i = i + 1
+print("End")
+
+
+# 12
+print("Start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop starts with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i} j: {j}')
+    j= j+1
+  i = i+1
+print("End")
+
+
+#13
+print("Start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop start with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i} j: {j}')
+    j = j+1
+  i = i+1
+print("End")
+
+
+#14 
+print("Start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop starts with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i} j: {j}')
+    j = j + 1
+  i = i + 1
+print("End")
+
+
+#15
+print("Start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop start with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i} j:{j}')
+    j = j + 1
+  i = i + 1
+print("End")
+
+
+#16
+print("Start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop start with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i} j: {j}')
+    j = j + 1
+  i = i + 1
+print("Start")
+
+#17
+print("Start")
+N = 5
+i = 0
+while i < N:
+  print("outer loop start with i ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i} j: {j}')
+    j = j + 1
+  i = i + 1
+print("End")
+
+# 18
+print("Start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop start with i: ", i)
+  j= 0
+  while j < N:
+    print(f'i: {i} j: {j}')
+    j = j+1
+  i = i + 1
+print("End")
+
+
+#19
+print("Start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop variable starts with i: ", i)
+  j = 0
+  while j < N:
+    print(f'i: {i}' j: {j})
+    j = j + 1
+  i = i + 1
+print("End")
+
+
+#20
+print("Start")
+N = 5
+i = 0
+while i < N:
+  print("Outer loop variable start with i: ", i)
+  j= 0
+  while j < N:
+    print(f'i: {i} j: {j}')
+    j = j + 1
+  i = i + 1
+print("End")
